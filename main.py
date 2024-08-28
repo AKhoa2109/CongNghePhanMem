11c10759443b790815985f4f70a74ccd3bb7693a
AKhoa2109 <anhkhoa2109.it@gmail.com>
2024-08-28 09:21:38 +0700

    Update name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import torch
-
+print("Nguyễn Hoàng Anh Khoa. MSSV: 22110352")
 print("Welcome the MyClip project.")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print('Use device: ',device)
